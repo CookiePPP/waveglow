1eabe89e27def3134ad59a058630ad26c8f65845
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-01-14 04:27:19 +0000

    typo

--- mel2samp.py
+++ mel2samp.py
@@ -64,7 +64,7 @@ class Mel2Samp(torch.utils.data.Dataset):
     spectrogram, audio pair.
     """
     def __init__(self, training_files, segment_length, filter_length,
-                 hop_length, win_length, sampling_rate, mel_fmin, mel_fmax, load_mel_from_dist):
+                 hop_length, win_length, sampling_rate, mel_fmin, mel_fmax, load_mel_from_disk):
         self.audio_files = files_to_list(training_files)
         random.seed(1234)
         random.shuffle(self.audio_files)
